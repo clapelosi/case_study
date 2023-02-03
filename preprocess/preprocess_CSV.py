3a18b6903ec53388cbeb1da95621e3439f5c1a6a
clapelosi <cla.pelosi.10@gmail.com>
2023-02-03 15:54:39 +0100

    added the two files to preprocess the csv and xmnl file

---
+++ preprocess/preprocess_CSV.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+from support_functions import trans
+import os 
+os.chdir("./case_study")
+
+transaction_df = pd.read_csv('case_study/data/Transazioni_Azienda_ABC.csv')
+transaction_df.rename(columns = {'data ':'date', 'categoria':'category', 'amount':'amt'}, inplace = True)
+transaction_df['date'] = pd.to_datetime(transaction_df['date'], format="%d/%m/%Y")
+transaction_df.replace(to_replace=r'€ ', value='', regex=True, inplace=True)
+transaction_df['amt'].replace(to_replace=r'\.', value= '', regex=True, inplace=True)
+transaction_df['amt'].replace(to_replace=r',', value= '.', regex=True, inplace=True)
+transaction_df['amt'] = transaction_df['amt'].astype('float64')
+
+trans_transformed1_df = pd.get_dummies(transaction_df, columns=['category'])
+
+trans_transformed1_df.to_csv('./cleaned_data/trans_transformed1.csv', index=False, header=True)
+
+
+for i in range(2,trans_transformed1_df.shape[1]):
+    trans_transformed2_df = trans(trans_transformed1_df,i,1)
+
+
+
+trans_transformed2_df.rename(columns = {'category_EROGAZIONE FINANZIAMENTO':'loan_disbursement', 
+                                        'category_INCASSO FATTURA':'bill_incomes', 
+                                        'category_PAGAMENTO FATTURA':'bill_outcomes',
+                                        'category_PAGAMENTO FORNITORI':'supplier_outcomes',
+                                        'category_RIMBORSO FINANZIAMENTO':'loan_repayments',
+                                        'category_STIPENDI':'salaries',
+                                        'category_UTENZE':'utilities', 'amount':'amt'}, inplace = True)
+
+trans_transformed2_df.drop(columns=['amt'], inplace=True)
+
+trans_transformed2_df.to_csv('./cleaned_data/trans_transformed2.csv', index=False, header=True)
+
+
+
---
+++ preprocess/preprocess_XBRL.py
@@ -0,0 +1,63 @@
+from bs4 import BeautifulSoup, NavigableString, Tag 
+import pandas as pd
+import numpy as np
+from support_functions import clean_text, structure_xml, get_attribs_val, get_omogeneus_data
+
+with open('./data/bilancio_test.xml', 'r') as f:
+    file = f.read()
+soup = BeautifulSoup(file, 'xml')
+xbrl = soup.body.xbrl
+
+obeject_des= []
+attrib_values_raw = []
+values_raw = []
+for child in xbrl.children:
+    if isinstance(child, NavigableString):
+        continue
+    if isinstance(child, Tag):
+        obeject_des.append(child.name)
+        attrib_values_raw.append(child.attrs)
+        values_raw.append(child.text)      
+
+attrib_values = []
+for val in attrib_values_raw:
+    attrib_values.append(get_attribs_val(val))
+
+values = []
+for val in values_raw:
+    values.append(clean_text(val))
+
+for i in range(len(attrib_values)):
+    attrib_values[i].append(obeject_des[i])
+    attrib_values[i].append(values[i])
+  
+
+a = attrib_values[5 :]
+
+lengths = list(set(map(len, a)))
+      
+
+
+data_by_length = []
+for i in range(len(lengths)):
+    data_by_length.append(get_omogeneus_data(a, lengths[i]))
+#    exec(f'data_{i} = data_i')
+
+
+header_3 = ['contextref', 'voice', 'description']
+header_5 = ['contextref', 'decimals', 'unitref', 'voice', 'amount']
+header_6 = ['contextref', 'decimals', 'nazione', 'unitref', 'voice', 'amount']
+
+master_fin_stat_raw_df = pd.DataFrame(data_by_length[0], columns=header_3)
+main_fin_stat_raw_df = pd.DataFrame(data_by_length[1], columns=header_5)
+main_fin_stat_raw_2_df = pd.DataFrame(data_by_length[2], columns=header_6)
+
+master_fin_stat_raw_df.to_csv('./cleaned_data/master_fin_stat_raw.csv', index=False, header=True)
+main_fin_stat_raw_df.to_csv('./cleaned_data/main_fin_stat_raw.csv', index=False, header=True)
+main_fin_stat_raw_2_df.to_csv('./cleaned_data/main_fin_stat_raw_2.csv', index=False, header=True)
+
+#a = structure_xml(xbrl)
+print('this is th efirst output:', xbrl.find_all('context') ,'\n')
+
+print('this is the second output:',list(xbrl.children))
+  
